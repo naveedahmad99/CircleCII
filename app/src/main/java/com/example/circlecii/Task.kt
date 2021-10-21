4e130549309b5fa194b9c5465b1696105ddc9bc9
naveedahmad <sr.naveed99@gmail.com>
2021-10-21 21:10:23 +0500

    sonar

---
+++ app/src/main/java/com/example/circlecii/Task.kt
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.circlecii
+import java.util.UUID
+
+/**
+ * Immutable model class for a Task. In order to compile with Room, we can't use @JvmOverloads to
+ * generate multiple constructors.
+ *
+ * @param title       title of the task
+ * @param description description of the task
+ * @param isCompleted whether or not this task is completed
+ * @param id          id of the task
+ */
+data class Task @JvmOverloads constructor(
+    var title: String = "",
+    var description: String = "",
+    var isCompleted: Boolean = false,
+    var id: String = UUID.randomUUID().toString()
+) {
+
+    val titleForList: String
+        get() = if (title.isNotEmpty()) title else description
+
+
+    val isActive
+        get() = !isCompleted
+
+    val isEmpty
+        get() = title.isEmpty() || description.isEmpty()
+}
---
+++ app/src/test/java/com/example/circlecii/StatisticsUtilsKtTest.kt
@@ -0,0 +1,21 @@
+package com.example.circlecii
+
+import junit.framework.TestCase
+
+class StatisticsUtilsKtTest : TestCase() {
+
+    fun testGetActiveAndCompletedStats() {
+
+        // Create an active task (the false makes this active)
+        val tasks = listOf<Task>(
+            Task("title", "desc", isCompleted = false)
+        )
+        // Call your function
+        val result = getActiveAndCompletedStats(tasks)
+
+        // Check the result
+        assertEquals(result.completedTasksPercent, 0f)
+        assertEquals(result.activeTasksPercent, 100f)
+//        assertEquals(4,2+2)
+    }
+}
